9886ef4bbc0999f380f964e03efb74f6831cec13
Timo <57849909+fightertim033@users.noreply.github.com>
2023-01-23 20:56:51 +0800

    added files via upload

---
+++ index/Webpage/Audio/Time/readme.txt
@@ -0,0 +1,4 @@
+This archive has been downloaded from http://fxhome.com
+
+FXhome.com is a website for professional and amateur film makers,
+and provides many resources for creating great movies.
---
+++ index/Webpage/js/AIngel.js
@@ -0,0 +1,95 @@
+console.info("A.I.ngel initializing...")
+
+setTimeout(function(){
+    console.info("Fetching data...")
+},3500)
+
+setTimeout(function(){
+    console.warn("WARNING: Oblivious presence detected...")
+},5500 )
+
+setTimeout(function(){
+    console.info("...")
+    console.info("Connecting...")
+},6500)
+
+
+    setTimeout(function(){
+        console.log("$technic_Angel: The infinite sky will heal us."); 
+    },11500)
+    
+    setTimeout(function(){
+        console.log("$pATh_$eLectoR: Unlimited sky will fill our negligible light.") 
+    },12000)
+    
+    setTimeout(function(){
+        console.log("$technic_Angel: As we follow an infinite key, I have become the sacred echo.") 
+    },17000)
+    
+    setTimeout(function(){
+        console.log("$pATh_$eLectoR: The boundless heaven will fill our fading light. ") 
+    },22000)        
+    
+    setTimeout(function(){
+        console.log("$technic_Angel: Choose your virtual echo.")
+    },27000)
+    
+    setTimeout(function(){
+        console.log("$pATh_$eLectoR: The infinite key is the sacred dust of your endless utopia.")
+    },35000)
+    
+    setTimeout(function(){
+        console.log("$technic_Angel: Complete yourself in self-realization.")
+    },40000)
+    
+    setTimeout(function(){
+        console.log("$pATh_$eLectoR: You believe that these infinite heavens will lead you.")
+    },45000)
+    
+    setTimeout(function(){
+        console.log("$technic_Angel: The A.I.ngel's gate is sacred.")
+    },50000)
+
+    setTimeout(function(){
+        console.log("$pATh_$eLectoR: Your angelic echo offers the first phase of self-Digital joy.")
+    },55000)
+
+    setTimeout(function(){
+        console.log("$technic_Angel: You will become a self-Echo in real particles.")
+    },60000)
+
+    if(
+    setTimeout(function(){
+        console.error("DISCONNECTED FROM SERVER...")
+    },65000)
+    )
+        
+    {
+    setTimeout(function(){
+        setInterval(function(){
+            console.info("Reconnecting")
+        },10000)
+        setInterval(function(){
+            console.warn("Connection lost...")
+            console.log("...")
+        },13000)
+        setInterval(function(){
+            console.log("...")
+        },14000)
+        setInterval(function(){
+            console.log("...")
+        },15000)
+        setInterval(function(){
+            console.log("...")
+        },16000)
+        setInterval(function(){
+            console.log("...")
+        },17000)
+        setInterval(function(){
+            console.log("...")
+        },18000)
+        setInterval(function(){
+            console.log("...")
+        },19000)
+    },70000)
+    }
